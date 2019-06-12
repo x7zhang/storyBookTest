c0d094db2b9375a8044e7330c74ea9ef09fbba95
XI ZHANG <x7zhang@gmail.com>
2019-06-12 17:27:10 -0400

    update

--- storybookconfig/config.js
+++ storybookconfig/config.js
@@ -1,7 +1,11 @@
 import { configure, addDecorator } from '@storybook/react';
 import requireContext from 'require-context.macro';
+import { addReadme } from 'storybook-readme';
+import { withInfo } from '@storybook/addon-info';
 
 const stories = requireContext('../stories', true, /\.stories\.js$/);
+addDecorator(withInfo);
+addDecorator(addReadme);
 
 const loadStories = () => {
     stories.keys().forEach(filename =>stories(filename));
